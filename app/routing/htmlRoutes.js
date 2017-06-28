cf0a4eb654fb2c2d1fcb60ac346a4d8bbf85a019
gabietaylor <gabie.taylor@yahoo.com>
2017-06-28 16:12:08 -0400

    minor chgs

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,19 +2,15 @@
 var path = require('path');
 
 module.exports = function (app) {
-
     app.get("/", function(req, res) {
         res.sendFile(path.join(__dirname, '/../public/home.html'));
     });
-
     app.post('/', function (req, res) {
         console.log(req.body)
     });
-
     app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, '/../public/survey.html'));
     });
-
     app.post('/survey', function (req, res) {
         console.log(req.body)
     });
